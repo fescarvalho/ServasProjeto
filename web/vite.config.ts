31dcdd6f92bcb4c25440db8a50d81de6d89fd932
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2026-01-21 12:05:55 -0300

    FIX: Correçoes no PWA

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "prompt", // <--- MUDANÇA AQUI: Agora ele espera o usuário confirmar
+      registerType: "autoUpdate", // MUDE TEMPORARIAMENTE PARA autoUpdate
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      // Adicione estas configurações de workbox para forçar a limpeza
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
+        sourcemap: true
+      },
       manifest: {
         name: "Escala das Servas",
         short_name: "Servas do Altar",
@@ -27,14 +33,11 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
         ],
       },
     }),
   ],
+  build: {
+    target: 'es2020', // <--- Garanta que está es2020 ou esnext
+  }
 });
